6c1b87a9ae635453363ec82ed0ffe11678f8bb68
Kon Spirin <kon@mail.com>
2025-08-12 19:45:19 +0300

    cw11: async refactoring interface and business logic for array saving

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -8,16 +8,16 @@ import {convertBookDtoToBook} from "../utils/tools.js";
 export class BookController {
     private libService: LibService = libServiceEmbedded;
 
-    getAllBooks(req:Request, res:Response){
-        const result = this.libService.getAllBooks();
+    async getAllBooks(req: Request, res: Response) {
+        const result = await this.libService.getAllBooks();
         res.json(result);
     }
 
-    addBook(req:Request, res:Response){
+    async addBook(req: Request, res: Response) {
         const dto = req.body as BookDto;
-        const book:Book = convertBookDtoToBook(dto);
-        const result = this.libService.addBook(book);
-        if(result)
+        const book: Book = convertBookDtoToBook(dto);
+        const result = await this.libService.addBook(book);
+        if (result)
             res.status(201).json(book)
         else throw new HttpError(409, 'Book not added. Id conflict')
     }
