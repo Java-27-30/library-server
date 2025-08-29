5c4dd07f44eca6093920db8a6c16efbf2696b206
Kon Spirin <kon@mail.com>
2025-08-29 11:04:36 +0300

    cw15: configurations from .json

--- src/config/libConfig.ts
+++ src/config/libConfig.ts
@@ -1,46 +1,75 @@
-import mysql from 'mysql2/promise'
+import mysql, {Pool} from 'mysql2/promise'
 import dotenv from 'dotenv'
-import {Roles} from "../utils/libTypes.js";
+import appConf from "../../app-config/app-config.json";
 
+export interface AppConfig {
+    port:number,
+    skipRoutes:string[],
+    pathRoles: Record<string, string[]>,
+    checkIdRoutes:string[],
+    pool: Pool,
+    mongoUri:string
 
-export const PORT=3500;
-export const MONGO_URI = 'mongodb+srv://konspirin:lEUH2CDZKqCvxdCx@cluster0.chmbb0u.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster0'
+}
+//export const PORT=3500;
+//export const MONGO_URI = 'mongodb+srv://konspirin:lEUH2CDZKqCvxdCx@cluster0.chmbb0u.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster0'
 dotenv.config();
-//===============mySQL Connection================
-export const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT!,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-})
 
-export const SKIP_ROUTES = [
-    "POST/accounts", "GET/api/books"
-]
+export const configuration:AppConfig = {
+    ...appConf,
+    pool: mysql.createPool({
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT!,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    }),
+    mongoUri: process.env.MONGO_URI || "dev db address"
+}
+
+
+
+
+
+
+
+
+
+//===============mySQL Connection================
+// export const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: +process.env.DB_PORT!,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD
+// })
+
+// export const SKIP_ROUTES = [
+//     "POST/accounts", "GET/api/books"
+// ]
 /*
     USER: getAccById(?), changePass(?), updateAcc(?) || HW || getBookById getBookByJenre
     ADMIN: getAccById, updateAcc, addBook, removeBook, getBookById, getBooksByReader
     SUPERVISOR: deleteAcc, changeRoles
     LIBRARIAN: pickUpbook, returnBook, getters
  */
-export const PATH_ROUTES = {
- "GET/accounts/reader_id" : [Roles.USER, Roles.ADMIN],
-    "PATCH/accounts/password" : [Roles.USER],
-    "DELETE/accounts" : [Roles.SUPERVISOR],
-    "PATCH/accounts" : [Roles.USER,Roles.ADMIN],
-    "PUT/accounts/roles" : [Roles.SUPERVISOR],
-    //===========================HW==================
-    "GET/api/books/book_id" : [Roles.USER, Roles.ADMIN],
-    "GET/api/books/genre" : [Roles.USER, Roles.LIBRARIAN],
-    "GET/api/books/gen_st" : [Roles.LIBRARIAN],
-    "POST/api/books":[Roles.ADMIN],
-    "DELETE/api/books":[Roles.ADMIN],
-    "PATCH/api/books/pickup":[Roles.LIBRARIAN],
-    "PATCH/api/books/return":[Roles.LIBRARIAN],
-    "GET/api/books/books_reader":[Roles.ADMIN, Roles.LIBRARIAN]
-}
+// export const PATH_ROUTES = {
+//  "GET/accounts/reader_id" : [Roles.USER, Roles.ADMIN],
+//     "PATCH/accounts/password" : [Roles.USER],
+//     "DELETE/accounts" : [Roles.SUPERVISOR],
+//     "PATCH/accounts" : [Roles.USER,Roles.ADMIN],
+//     "PUT/accounts/roles" : [Roles.SUPERVISOR],
+//     //===========================HW==================
+//     "GET/api/books/book_id" : [Roles.USER, Roles.ADMIN, Roles.LIBRARIAN],
+//     "GET/api/books/genre" : [Roles.USER, Roles.LIBRARIAN],
+//     "GET/api/books/gen_st" : [Roles.LIBRARIAN],
+//     "POST/api/books":[Roles.ADMIN],
+//     "DELETE/api/books":[Roles.ADMIN],
+//     "PATCH/api/books/pickup":[Roles.LIBRARIAN],
+//     "PATCH/api/books/return":[Roles.LIBRARIAN],
+//     "GET/api/books/books_reader":[Roles.ADMIN, Roles.LIBRARIAN]
+// }
 
-export const CHECK_ID_ROUTES = [
-    "GET/accounts/reader_id", "PATCH/accounts/password", "PATCH/accounts"
-]
+// export const CHECK_ID_ROUTES = [
+//     "GET/accounts/reader_id", "PATCH/accounts/password", "PATCH/accounts"
+// ]
--- src/services/libServiseImplSQL.ts
+++ src/services/libServiseImplSQL.ts
@@ -1,13 +1,14 @@
 import {LibService} from "./libService.js";
 import {Book, BookGenres, BookStatus} from "../model/Book.js";
-import {pool} from "../config/libConfig.js";
+import {configuration} from "../config/libConfig.js";
 import {HttpError} from "../errorHandler/HttpError.js";
 import {Reader} from "../model/Reader.js";
+
 
 export class LibServiceImplSQL implements  LibService{
 
     async addBook(book: Book): Promise<boolean> {
-        const result = await pool.query('INSERT INTO books VALUES(?,?,?,?,?)',
+        const result = await configuration.pool.query('INSERT INTO books VALUES(?,?,?,?,?)',
             [book.id, book.title, book.author, book.genre, book.status])
        // console.log(result)
         if(!result)
@@ -16,12 +17,12 @@ export class LibServiceImplSQL implements  LibService{
     }
 
     async getAllBooks(): Promise<Book[]> {
-        const [result] = await pool.query('SELECT * FROM books')
+        const [result] = await configuration.pool.query('SELECT * FROM books')
         return result as unknown as Book[];
     }
 
     async getBooksByGenre(genre: BookGenres): Promise<Book[]> {
-        const [result] = await pool.query('SELECT * from books WHERE genre = ?', [genre])
+        const [result] = await configuration.pool.query('SELECT * from books WHERE genre = ?', [genre])
         return result as Book[];
     }
 
@@ -34,19 +35,19 @@ export class LibServiceImplSQL implements  LibService{
         if (!book)
             throw new HttpError(400,"Can't return book because this book or reader not exists");
         if (!queriedReader) {
-            await pool.query('INSERT INTO readers VALUES (null, ?)', [reader]);
+            await configuration.pool.query('INSERT INTO readers VALUES (null, ?)', [reader]);
             queriedReader = await this.getReaderByName(reader.userName);
         }
 
-        await pool.query('INSERT INTO books_readers VALUES(?, ?, ?, ?)',
+        await configuration.pool.query('INSERT INTO books_readers VALUES(?, ?, ?, ?)',
             [book.id, queriedReader.reader_id, new Date().toDateString(), null]);
-        pool.query('UPDATE books SET status = "on_hand" WHERE id = ?', [id])
+        configuration.pool.query('UPDATE books SET status = "on_hand" WHERE id = ?', [id])
 
     }
 
     private getReaderByName = async (reader: string) => {
         const query = 'SELECT * FROM readers WHERE name = ?'
-        const [result] = await pool.query(query, [reader]);
+        const [result] = await configuration.pool.query(query, [reader]);
         const readersArr = result as { reader_id: number, name: string }[];
         const [queriedReader] = readersArr;
         return queriedReader;
@@ -57,8 +58,8 @@ export class LibServiceImplSQL implements  LibService{
         if (book.status !== BookStatus.ON_STOCK)
             throw new HttpError(400, "Wrong book status. Book on hand yet");
         book.status = BookStatus.REMOVED;
-        pool.query('DELETE FROM books_readers WHERE book_id = ?', [id])
-        pool.query('DELETE FROM books WHERE id = ?', [id])
+        configuration.pool.query('DELETE FROM books_readers WHERE book_id = ?', [id])
+        configuration.pool.query('DELETE FROM books WHERE id = ?', [id])
         return book;
     }
 
@@ -66,14 +67,14 @@ export class LibServiceImplSQL implements  LibService{
         const book = await this.getBookById(id);
         if (book.status !== BookStatus.ON_HAND)
             throw new HttpError(400,"Wrong book status");
-        pool.query('UPDATE books SET status = "on_stock" WHERE id = ?', [id])
-        await pool.query('UPDATE books_readers SET return_date = ? WHERE book_id = ? AND return_date IS NULL', [new Date().toDateString(), id]);
+        configuration.pool.query('UPDATE books SET status = "on_stock" WHERE id = ?', [id])
+        await configuration.pool.query('UPDATE books_readers SET return_date = ? WHERE book_id = ? AND return_date IS NULL', [new Date().toDateString(), id]);
     }
 
     async getBookById(id: string): Promise<Book> {
         const query = 'SELECT * FROM books WHERE id = ?';
         const value = [id]
-        const [result] = await pool.query(query, value);
+        const [result] = await configuration.pool.query(query, value);
         const books = result as Book[];
 
         if (books.length) {
@@ -91,7 +92,7 @@ export class LibServiceImplSQL implements  LibService{
 
     getPickRecordsByBookId = async (id: string) => {
         const query = 'SELECT pick_date, return_date, name as readerName FROM (books_readers as b_r JOIN readers as r ON b_r.reader_id = r.reader_id) WHERE b_r.book_id = ?'
-        const [result] = await pool.query(query, [id])
+        const [result] = await configuration.pool.query(query, [id])
         return result as {pick_date: string, return_date:string, readerName: string, readerId: 101010101}[];
     }
 
